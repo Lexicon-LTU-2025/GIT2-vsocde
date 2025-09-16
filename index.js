bbb2dc66803c0461a488cd9becb520f556b2d5d7
Niklas Fähnrich <niklas.fahnrich@safeture.com>
2025-09-16 10:30:52 +0200

    create greet function and check isPerson

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const fullname = `${firstName} ${lastName}`;
 
 console.log('Name:', fullname);
 console.log('Firstname and Lastname:', firstName, lastName);
+
+const greet = (fullname) => {
+  const isPerson = true;
+
+  console.log(`Hello! Are you: ${fullname}. Is a person: ${isPerson}`)
+}
